0a6660c4fde5e767e00476b565c188930478ab46
ViMonks <60519132+ViMonks@users.noreply.github.com>
2020-04-29 14:36:00 -0400

    Update loot_totals.py

--- loot_totals.py
+++ loot_totals.py
@@ -1,105 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import json
 import csv
 import numpy as np
 import datetime as dt
 import pandas as pd
 
-def player_loot(name):
-	loot = []
-	for drop in data:
-		if name.title() in drop["player"] and not drop["response"] == "Disenchant":
-			loot.append(items_dict[str(drop["itemID"])])
-	loot.sort()
-	return(f"{name.title()} has received {len(loot)} items.", loot)
-
-def class_average_loot(name):
-	roster = []
-	player_class = ""
-	for drop in data:
-		if name.title() in drop["player"]:
-			player_class=drop["class"]
-			break
-	for drop in data:
-		if drop["class"]==player_class and (drop["player"].split("-")[0]).lower() in raiders and not drop["response"] == "Disenchant":
-			roster.append(drop["player"])
-	total_items_for_class=len(roster)
-	number_of_class=len(set(roster))
-	return total_items_for_class/number_of_class
-
-
-class Member:
-	def __init__(self, name):
-		self.name=name
-		self.items=["test_item"]
-
-raiders = ["ariadnye", "fawyn", "floramel", "mav", "zallai", "aimelia", "evindel", "steadyshot", "tower", "fiz", "rancelor",
-		   "relink", "sessnishi", "exaethian", "gildi", "gordy", "kyra", "payge", "violily", "yungdingo", "bubblywrap", "elluna",
-		   "hitomi", "holymedic", "skorgson", "spri", "lumberfoot", "mackdagger", "noxxious", "nyhthraefn", "theodora", "vihgo",
-		   "akku", "hiriko", "jank", "kaydra", "kylace", "xilus", "alunys", "benecia", "caritheri", "gossow", "meadowglen",
-		   "myrd", "sanno", "xandin"]
-data = json.load(open("export.json", "r"))
-
 with open("items.csv") as f:
 	next(f)
 	reader = csv.reader(f, skipinitialspace=True)
 	items_dict = dict(reader)
 
-
-# In[2]:
-
-
 df = pd.read_json("export.json")
 
-
-# In[3]:
-
-
-df = df[df.response != 'Disenchant'] # removing disenchanting and banking rolls
+# Removing rolls that aren't standard loot distributions. For my guild, this included disenchanting and guild banking rolls.
+# Update the 'Disenchant' and 'Banking' text strings to reflect the names of rolls you would like to exclude for your guild.
+# These lines won't cause any problem if your guild doesn't use these kinds of rolls. You can leave them in place.
+df = df[df.response != 'Disenchant']
 df = df[df.response != 'Banking']
-
-
-# In[4]:
 
 
 df["itemID"] = df["itemID"].astype(str)
 df["item"] = df["itemID"].map(items_dict) # turning the item column, which contains itemID, into item name
 
-
-# In[5]:
-
-
 df = df[~df.item.str.contains("Pattern|Formula|Recipe|Schematic|Plans|Tranquilizing")] # removing crafting pattersn and tome of tranq
 
-
-# In[6]:
-
-
 df.drop(["time", "itemString", "votes", "boss", "isAwardReason", "subType", "equipLoc", "note", "owner", "itemID"], axis=1, inplace=True)
-
-
-# In[7]:
-
 
 df['player'] = df['player'].map(lambda x: x.split("-")[0]) # removing the realm name after player name
 
 
-# In[8]:
-
-
-grouped = df.groupby("date")
-
-
-# In[9]:
-
-
 # generating the list of items per date
 
+grouped = df.groupby("date")
 drops_db=pd.DataFrame()
 drops_db['player'] = list(set(df['player']))
 drops_db.set_index("player", inplace=True)
@@ -113,10 +47,8 @@ for name, group in grouped: # name is the date, group is the dataframe filtered 
 drops_db.sort_index(inplace=True)
 
 
-# In[10]:
-
-
 # generating count of total items received
+
 total_column = []
 
 for player in drops_db.index:
@@ -128,9 +60,4 @@ for player in drops_db.index:
 drops_db.columns = pd.to_datetime(drops_db.columns).date
 drops_db['total'] = total_column
 
-
-# In[11]:
-
-
-drops_db.to_csv("remnant_loot.csv")
-
+drops_db.to_csv("loot_history.csv")
